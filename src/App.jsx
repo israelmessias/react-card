50778ff326bdbd6b8e020c2d0ab81977f22d500f
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-07-30 10:13:51 -0300

    Correção

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Card from './Components/layout/Card'
 
 export default () => {
-    <div id="app">
-        <Card></Card>
-    </div>
+    return <div id="app">
+            <Card></Card>
+           </div>
 };
