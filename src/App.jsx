972d346631b595db8905ff15259d85b5d955ecf2
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-08-05 17:58:22 -0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./style.css";
 import Card from "./Components/layout/Card";
+import Media from './Components/Aleatorio'
 
 export default () => {
   return (
     <div id="app">
-      <Card titulo="Exemplo de Card"></Card>
+      <Card titulo="Exemplo de Card">
+        <Media max={15} min={2}></Media>
+      </Card>
     </div>
   );
 };
